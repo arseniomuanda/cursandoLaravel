b7fb545978a68e897b4640933722e2d1748e9084
Arsénio Muanda <66343929+arseniomuanda@users.noreply.github.com>
2023-11-28 19:13:44 +0100

    adicionar factory para categoreis

--- database/seeders/CategoreisSeeder.php
+++ database/seeders/CategoreisSeeder.php
@@ -2,25 +2,22 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-
-class UserSeeder extends Seeder
+class CategoreisSeeder extends Seeder
 {
     protected $model;
     function __construct()
     {
-        $this->model = new User();
+        $this->model = new Category();
     }
-
-
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $this->model->factory(34)->create();
+        $this->model->factory(5)->create();
     }
 }
