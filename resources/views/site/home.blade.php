1e4ff8ff4195fb48780f1c7b69b42265d591d8d1
Arsénio Muanda <66343929+arseniomuanda@users.noreply.github.com>
2023-12-11 00:46:39 +0100

    operador ternario

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -4,4 +4,20 @@
 
 @section('content')
     <h1>My content hire... </h1>
+
+    {{ isset($nome) ? "Esta certo" : "errado"}}
 @endsection
+
+{{-- Aqui está uma lista de alguns comandos do Blade no Laravel:
+
+@extends: Usado para estender um layout mestre em uma visualização.
+@section: Usado para definir uma seção de conteúdo em um layout mestre ou visualização.
+@yield: Usado para exibir o conteúdo de uma seção definida em um layout mestre.
+@include: Usado para incluir uma subvisualização em uma visualização.
+@if, @elseif, @else: Usados para adicionar estruturas condicionais em uma visualização.
+@foreach, @for, @while: Usados para adicionar estruturas de repetição em uma visualização.
+@isset, @empty: Usados para verificar se uma variável está definida ou vazia.
+@auth, @guest: Usados para verificar se o usuário está autenticado ou não.
+@component: Usado para renderizar um componente Blade.
+@stack: Usado para empilhar conteúdo em uma seção específica.
+Esses são apenas alguns dos comandos mais comumente usados no Blade do Laravel. Existem muitos outros comandos e recursos disponíveis para ajudar na criação de visualizações dinâmicas e reutilizáveis. --}}
