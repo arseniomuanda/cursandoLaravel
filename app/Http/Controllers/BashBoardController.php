c5d235ca68f056d607cd527cd69e0d296ed19ddb
Arsénio Muanda <66343929+arseniomuanda@users.noreply.github.com>
2023-12-19 00:30:10 +0100

    tipos de aplicaçao de Middlewares

--- app/Http/Controllers/BashBoardController.php
+++ app/Http/Controllers/BashBoardController.php
@@ -8,6 +8,7 @@ class BashBoardController extends Controller
 {
     public function __construct()
     {
+        //Tambem podemos usar uma lista de metodos dentro do only ou usar o except
         $this->middleware('auth')->only('index');
     }
     public function index()
