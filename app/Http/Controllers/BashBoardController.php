ecaddb9e432fa9a2c1c23de3e69c31bf0be469fc
Arsénio Muanda <66343929+arseniomuanda@users.noreply.github.com>
2023-12-27 18:01:22 +0100

    contagem de users

--- app/Http/Controllers/BashBoardController.php
+++ app/Http/Controllers/BashBoardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BashBoardController extends Controller
 {
@@ -10,10 +13,43 @@ class BashBoardController extends Controller
     {
         //Tambem podemos usar uma lista de metodos dentro do only ou usar o except
         $this->middleware(['auth', 'checkemail'])->only('index');
-
     }
     public function index()
     {
-        return view('admin.dashboard');
+        $users = User::all()->count();
+        $productsData = Produto::select(
+            'categories.name AS category',
+            DB::raw('COUNT(*) as total')
+        )
+            ->join('categories', 'produtos.cat', '=', 'categories.id')
+            ->groupBy('categories.name')
+            ->get();
+
+
+        foreach ($productsData as $value) {
+            $productLabals[] = $value->category;
+            $productTotals[] = $value->total;
+        }
+
+        $usersData = User::select(
+            DB::raw('MONTH(created_at) as month'),
+            DB::raw('COUNT(*) as total')
+        )->groupBy('month')
+            ->orderBy('month', 'asc')->where('YEAR(created_at)', date('Y'))->get();
+
+        foreach ($usersData as $value) {
+            $userLabels[] = $value->category;
+            $userTotals[] = $value->total;
+        }
+
+        #processa as variaveis convertendo em string
+        $productLabals = implode(',', $productLabals);
+        $productTotals = implode(',',  $productTotals);
+
+        $userLabel = 'Aquisição de utilizadores do ano de ' . date('Y');
+        $userLabels = implode(',', $userLabels);
+        $userTotals = implode(',',  $userTotals);
+
+        return view('admin.dashboard', compact('users', 'productsLabals'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -51,7 +51,7 @@
                 <article class="bg-gradient-blue card z-depth-4 ">
                     <i class="material-icons">face</i>
                     <p>Utilizadores</p>
-                    <h3>34 </h3>
+                    <h3>{{ $users }}</h3>
                 </article>
             </div>
 
@@ -83,3 +83,62 @@
         </section>
     </div>
 @endsection
+
+@push('graficos')
+    <script>
+        /* Gráfico 01 */
+        var ctx = document.getElementById('myChart');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['Janeiro', 'Feveiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro',
+                    'Outubro', 'Novembro', 'Dezembro'
+                ],
+                datasets: [{
+                    label: '2020',
+                    data: [12, 19, 3, 5],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+        /* Gráfico 02 */
+        var ctx = document.getElementById('myChart2');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Facebook', 'Google', 'Instagram'],
+                datasets: [{
+                    label: 'Visitas',
+                    data: [12, 19, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132)',
+                        'rgba(54, 162, 235)',
+                        'rgba(255, 159, 64)'
+                    ]
+                }]
+            }
+        });
+    </script>
+@endpush
