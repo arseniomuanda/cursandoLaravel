d0303066cdb2a49aff23245d28fbd3ea0dae1858
Arsénio Muanda <66343929+arseniomuanda@users.noreply.github.com>
2024-01-06 10:37:28 +0100

    ⚡️ getContent|subTotal|total

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    public function getContent(): array
+    {
+        return $this->where('user', auth()->id())->get();
+    }
+
+    public function subTotal(): float
+    {
+        return $this->quantity * $this->price;
+    }
+
+    public function total(): float
+    {
+        $total = 0;
+        foreach ($this->items as $item) {
+            $total += $item->subtotal();
+        }
+        return $total;
+    }
 }
